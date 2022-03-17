ec60ad63afb84deebede5a9a52f2ad770862a332
PapD1020 <papd@kkszki.hu>
2022-03-17 12:34:04 +0100

    validation and data communication with back-end works, login works

--- Szakdogához/CRUD-APPLICATION/client/src/routes/login.js
+++ Szakdogához/CRUD-APPLICATION/client/src/routes/login.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import { useForm } from "react-hook-form";
 import '../App.css';
 import Axios from 'axios';
 
@@ -10,6 +11,17 @@ export default function Login(){
     const [LoginStatus, setLoginStatus] = useState('');
 
     Axios.defaults.withCredentials = true;
+
+    const{
+        register,
+        handleSubmit,
+        formState:{errors}
+    } = useForm();
+
+    const onSubmit = (data) => {
+        //alert(JSON.stringify(data));
+        submitUserDataLogin();
+    }; // your form submit function which will invoke after successful validation
 
     const submitUserDataLogin = () => {
   
@@ -42,23 +54,49 @@ export default function Login(){
     }, []);
 
     return(
-        <div className="smallContainer">
-          <div className='form'>
-            <h3>Login</h3>
-                <label>UserUn</label>
-                <input type="text" name="userUn" onChange={(e) => {
-                    setUserUnLogin(e.target.value);
-                }}></input>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                    <label>User name:</label>
+                    <input type="text" className="form-control"{
+                        ...register("userName", {
+                            required: true,
+                            minLength: 3,
+                            maxLength: 30,
+                            pattern: /^[A-Za-z]+$/i //valószínűleg nincsenek benne ékezetes betűk, javítani kell
+                        })
+                    } onChange={(e) => {
+                        setUserUnLogin(e.target.value);
+                    }}/>
+                    
+                    {errors?.userName?.type === "required" && <div><h5>This field is required!</h5><p>Your must enter your user name.</p></div>}
+                    {errors?.userName?.type === "minLength" && <div><h5>Your user name is too short.</h5><p>Your user name length must be between 3 and 30 characters.</p></div>}
+                    {errors?.userName?.type === "maxLength" && <div><h5>Your user name is too long.</h5><p>Your user name length must be between 3 and 30 characters.</p></div>}
+                    {errors?.userName?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
+                </div>
 
-                <label>UserPw</label>
-                <input type="password" name="userPw" onChange={(e) => {
-                    setUserPwLogin(e.target.value);
-                }}></input>
+                <div className="form-group">
+                    <label>Password: </label>
+                    <input type="password" className="form-control"{
+                        ...register("userPw", {
+                            required: true,
+                            minLength: 8,
+                            maxLength: 16
+                        })
+                    } onChange={(e) => {
+                        setUserPwLogin(e.target.value);
+                    }}/>
 
-                <button className="btn" onClick={submitUserDataLogin}>Login</button>
-            </div>
+                    {errors?.userPw?.type === "required" && <div><h5>This field is required!</h5><p>Your must enter your password.</p></div>}
+                    {errors?.userPw?.type === "minLength" && <div><h5>Your password is too short.</h5><p>Your password length must be between 8 and 16 characters.</p></div>}
+                    {errors?.userPw?.type === "maxLength" && <div><h5>Your password is too long.</h5><p>Your password length must be between 8 and 100 characters.</p></div>}
+                </div>
 
-            <h1>{LoginStatus}</h1>
+
+                <h1>{LoginStatus}</h1>
+                
+                <input type="submit" value={"Login"}/> {/*Kell egybe ellenörző, küldő gomb vagy külön-külön ha nem megy egybe */}
+            </form>
         </div>
     );
 }
