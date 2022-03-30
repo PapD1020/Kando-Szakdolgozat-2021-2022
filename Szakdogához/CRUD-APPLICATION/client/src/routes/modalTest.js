6b51debe4e53b0daf858a74061870dbec61e0438
Pap <papd@kkszki.hu>
2022-03-30 13:00:22 +0200

    added code for modal body in comments

--- Szakdogához/CRUD-APPLICATION/client/src/routes/modalTest.js
+++ Szakdogához/CRUD-APPLICATION/client/src/routes/modalTest.js
@@ -82,6 +82,107 @@ export default function CreateArticle(){
     );
 }
 
+/*into modal body
+{ArticleNameList.map((val) => {
+            return(
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                  <label>Article type:</label>
+                  <select id="types" className="form-control" required {
+                      ...register("articleType", {
+                          required: true,
+                      })
+                  } onChange={(e) => {
+                      setArticleTypeUpd(e.target.value);
+                  }}>
+                      <option value={val.ArticleType}>{val.ArticleType}</option>
+                      <option value="Programming">Programming</option>
+                      <option value="Other">Other</option>
+                  </select>
+                  <div className="invalid-feedback">You must select a article type.</div>
+              </div>
+
+              <div className="form-group">
+                  <label>Article name:</label>
+                  <input type="text" className="form-control" value={val.ArticleName} {
+                      ...register("articleName", {
+                          required: true,
+                          minLength: 6,
+                          maxLength: 20,
+                          pattern: /^[A-Za-z]+$/i //valószínűleg nincsenek benne ékezetes betűk, javítani kell
+                      })
+                  }onChange={(e) => {
+                      setArticleNameUpd(e.target.value);
+                  }}/>
+                  
+                  {errors?.articleName?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a name.</p></div>}
+                  {errors?.articleName?.type === "minLength" && <div><h5>Your article's name is too short.</h5><p>Your article's name length must be between 6 and 20 characters.</p></div>}
+                  {errors?.articleName?.type === "maxLength" && <div><h5>Your article's name is too long.</h5><p>Your article's name length must be between 6 and 20 characters.</p></div>}
+                  {errors?.articleName?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
+              </div>
+              
+              <div className="form-group">
+                  <label>Article small description:</label>
+                  <input type="text" className="form-control" defaultValue={val.ArticleSmDescr} {
+                      ...register("articleSmDescr", {
+                          required: true,
+                          minLength: 8,
+                          maxLength: 100,
+                          pattern: /^[A-Za-z]+$/i //valószínűleg nincsenek benne ékezetes betűk, javítani kell
+                      })
+                  }onChange={(e) => {
+                      setArticleSmDescrUpd(e.target.value);
+                  }}/>
+
+                  {errors?.articleSmDescr?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a small description of your article.</p></div>}
+                  {errors?.articleSmDescr?.type === "minLength" && <div><h5>Your article's small description is too short.</h5><p>Your article's small description length must be between 8 and 100 characters.</p></div>}
+                  {errors?.articleSmDescr?.type === "maxLength" && <div><h5>Your article's small description is too long.</h5><p>Your article's small description length must be between 8 and 100 characters.</p></div>}
+                  {errors?.articleSmDescr?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
+              </div>
+
+              <div className="form-group">
+                  <label>Article main description:</label>
+                  <input type="text" className="form-control" defaultValue={val.ArticleMDescr} {
+                      ...register("articleMDescr", {
+                          required: true,
+                          minLength: 150,
+                          maxLength: 500,
+                          pattern: /^[A-Za-z]+$/i //valószínűleg nincsenek benne ékezetes betűk, javítani kell
+                      })
+                  }onChange={(e) => {
+                      setArticleMDescrUpd(e.target.value);
+                  }}/>
+
+                  {errors?.articleMDescr?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a main description of your article.</p></div>}
+                  {errors?.articleMDescr?.type === "minLength" && <div><h5>Your article's main description is too short.</h5><p>Your article's main description length must be between 150 and 500 characters.</p></div>}
+                  {errors?.articleMDescr?.type === "maxLength" && <div><h5>Your article's main description is too long.</h5><p>Your article's main description length must be between 150 and 500 characters.</p></div>}
+                  {errors?.articleMDescr?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
+              </div>
+
+              <div className="form-group">
+                  <label>Article image:</label>
+                  <input type="url" className="form-control" defaultValue={val.ArticleImg} {
+                      ...register("articleImg", {
+                          required: true, //kötelező legyen?
+                          minLength: 20, //Mennyi legyen?
+                          maxLength: 500, //Mennyi legyen?
+                      })
+                  }onChange={(e) => {
+                      setArticleImgUpd(e.target.value);
+                  }}/>
+
+                  {errors?.articleImg?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a picture</p></div>}
+                  {errors?.articleImg?.type === "minLength" && <div><h5>Your article's picture URL is too short.</h5><p>Your article's picture URL length must be between 150 and 500 characters.</p></div>}
+                  {errors?.articleImg?.type === "maxLength" && <div><h5>Your article's picture URL is too long.</h5><p>Your article's picture URL length must be between 150 and 500 characters.</p></div>}
+              </div>
+
+              <input type="submit" /> {/*Kell egybe ellenörző, küldő gomb vagy külön-külön ha nem megy egybe */}
+              </form>
+              ) 
+            })}
+
+*/
+
 //Modal pop up test. Works!
 /*
 import React, { Component } from "react";
