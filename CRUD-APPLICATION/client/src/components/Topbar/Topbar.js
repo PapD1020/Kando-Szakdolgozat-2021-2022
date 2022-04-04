d0ba7b53df860c1e166dd272d68c88d8eb0969da
tamasvar <vardai.tamas66@gmail.com>
2022-04-04 14:20:22 +0200

    04.04

--- CRUD-APPLICATION/client/src/components/Topbar/Topbar.js
+++ CRUD-APPLICATION/client/src/components/Topbar/Topbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./topbar.css";
 import {Outlet, Link} from 'react-router-dom';
 import { NotificationsNone, Language, Settings, Accessibility,AccountTree} from "@material-ui/icons";
-import { Button,Nav,Navbar, NavDropdown } from "react-bootstrap";
+import { Button,Nav,Navbar, NavDropdown,Container } from "react-bootstrap";
 
 export default function Topbar() {
   let user=JSON.parse(localStorage.getItem('token'))
@@ -12,35 +12,34 @@ export default function Topbar() {
   }
     return (
       <div >
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand href="">Navbar</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Nav className="mr-auto nav_bar_wrapper">
-            
+  
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="#home">Admin</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+                    <Link to="/articles"><Button>Home</Button></Link> 
+                    <Link to="/articleslist"><Button>Articleslist</Button></Link> 
+                    
+                    <Link to="/userlist"><Button>Userlist</Button></Link> 
+                    
               
-                  <Link to="/articles"><Button>Home</Button></Link> 
-                  <Link to="/articleslist"><Button>Articleslist</Button></Link> 
-                  
-                  <Link to="/userlist"><Button>Userlist</Button></Link> 
-                  
-             
-                  <Link to="/login"><Button>Login</Button></Link> 
-                  <Link to="/registration"><Button>Registration</Button></Link>
-              
+                    <Link to="/login"><Button>Login</Button></Link> 
+                    <Link to="/registration"><Button>Registration</Button></Link>
 
-            
-
-          </Nav>
           
-          <Nav className="justify-content-end">
-              <Link to="/profilePage"><img src=""/* {val.UserPP} */ alt=""/* {val.UserPP}  */className="topAvatar" /></Link>
-              <NavDropdown title="">
-                <NavDropdown.Item onClick={logOut}>LogOut</NavDropdown.Item>
-              </NavDropdown>
-          </Nav>
-          
-        </Navbar>
-       
-      </div>
+        </Nav>
+        <Nav>
+        <Link to="/profilePage"><img src=""/* {val.UserPP} */ alt=""/* {val.UserPP}  */className="topAvatar" /></Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item onClick={logOut}>LogOut</NavDropdown.Item>
+     
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+      </Navbar>
+</div>
     );
   }
