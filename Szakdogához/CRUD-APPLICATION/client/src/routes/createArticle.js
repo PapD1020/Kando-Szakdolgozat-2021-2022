60a9eb4e3c9f9e451b5314244d79b2e5b1847852
PapD1020 <papd@kkszki.hu>
2022-04-11 15:47:57 +0200

    createArticle.js frontend responsive, can add tooltips to small and main descr

--- Szakdogához/CRUD-APPLICATION/client/src/routes/createArticle.js
+++ Szakdogához/CRUD-APPLICATION/client/src/routes/createArticle.js
@@ -14,6 +14,8 @@ export default function CreateArticle(){
     
     const [LoginStatus, setLoginStatus] = useState('');
 
+    const [ErrorMessage, setErrorMessage] = useState('');
+
     Axios.defaults.withCredentials = true;
 
     const GotUserId = useRef(null);
@@ -73,104 +75,154 @@ export default function CreateArticle(){
     };
 
     return(
-        <div>
+        <div className="ms-3">
+            <div className="col-md-auto">
+                <h1 className="display-1 m-3">Create Article</h1>
+            </div>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
+            {LoginStatus && (
+                <div className="container">
+                    <form className="" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="row">
+                            <div className="col-lg-3">
+                                <div className="form-group">
+                                    <label className="display-6 mb-3">Article type:</label>
+                                    <select id="types" className="form-control p-2 mb-3"{
+                                        ...register("articleType", {
+                                            required: true,
+                                        })
+                                    } onChange={(e) => {
+                                        setArticleType(e.target.value);
+                                    }}>
+                                        <option value="" defaultValue={"Select the article type"}>Select the article type</option>
+                                        <option value="Programming">Programming</option>
+                                        <option value="Programming">Other</option>
+                                    </select>
+                                </div>
 
-                <div className="form-group">
-                    <label>Article type:</label>
-                    <select id="types" className="form-control" required {
-                        ...register("articleType", {
-                            required: true,
-                        })
-                    } onChange={(e) => {
-                        setArticleType(e.target.value);
-                    }}>
-                        <option value="" defaultValue={"Select the article type"}>Select the article type</option>
-                        <option value="Programming">Programming</option>
-                        <option value="Programming">Other</option>
-                    </select>
-                    <div className="invalid-feedback">You must select a article type.</div>
+                                <div className="errordiv text-danger mb-2">
+                                    {errors?.articleType?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a type.</p></div>}
+                                </div>
+                            </div>
+
+                            <div className="col-lg-3">
+                                <div className="form-group">
+                                    <label className="display-6 mb-3">Article name:</label>
+                                    <input type="text" className="mb-3 p-2 form-control"{
+                                        ...register("articleName", {
+                                            required: true,
+                                            minLength: 1,
+                                            maxLength: 20
+                                        })
+                                    } onChange={(e) => {
+                                        setArticleName(e.target.value);
+                                    }}/>
+                                </div>
+
+                                <div className="errordiv text-danger mb-2">
+                                    {errors?.articleName?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a name.</p></div>}
+                                    {errors?.articleName?.type === "minLength" && <div><h5>Your article's name is too short.</h5><p>Your article's name length must be between 6 and 20 characters.</p></div>}
+                                    {errors?.articleName?.type === "maxLength" && <div><h5>Your article's name is too long.</h5><p>Your article's name length must be between 6 and 20 characters.</p></div>}
+                                    {errors?.articleName?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
+                                </div>
+                            </div>
+
+                            <div className="col-lg-4">
+                                <div className="form-group">
+                                    <label className="display-6 mb-3">Article image:</label>
+                                    <input type="url" className="mb-3 p-2 form-control"{
+                                        ...register("articleImg", {
+                                            required: true,
+                                            minLength: 1,
+                                            maxLength: 500
+                                        })
+                                    } onChange={(e) => {
+                                        setArticleImg(e.target.value);
+                                    }}/>
+                                </div>
+
+                                <div className="errordiv text-danger mb-2">
+                                    {errors?.articleImg?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a picture</p></div>}
+                                    {errors?.articleImg?.type === "minLength" && <div><h5>Your article's picture URL is too short.</h5><p>Your article's picture URL length must be between 150 and 500 characters.</p></div>}
+                                    {errors?.articleImg?.type === "maxLength" && <div><h5>Your article's picture URL is too long.</h5><p>Your article's picture URL length must be between 150 and 500 characters.</p></div>}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-lg-5">
+                                <div className="form-group">
+                                    <label className="display-5 mb-3 mt-3">Article small description:</label>
+                                    <textarea rows="3" className="mb-3 p-2 form-control"{
+                                        ...register("articleSmDescr", {
+                                            required: true,
+                                            minLength: 1,
+                                            maxLength: 100
+                                        })
+                                    } onChange={(e) => {
+                                        setArticleSmDescr(e.target.value);
+                                    }}/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-sm-auto">
+                                <div className="errordiv text-danger mb-2">
+                                    {errors?.articleSmDescr?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a small description of your article.</p></div>}
+                                    {errors?.articleSmDescr?.type === "minLength" && <div><h5>Your article's small description is too short.</h5><p>Your article's small description length must be between 8 and 100 characters.</p></div>}
+                                    {errors?.articleSmDescr?.type === "maxLength" && <div><h5>Your article's small description is too long.</h5><p>Your article's small description length must be between 8 and 100 characters.</p></div>}
+                                    {errors?.articleSmDescr?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-sm-auto">
+                                <div className="form-group">
+                                    <label className="display-5 mb-3 mt-3">Article main description:</label>
+                                    <textarea rows="10" cols="170" className="mb-3 p-2 form-control"{
+                                        ...register("articleMDescr", {
+                                            required: true,
+                                            minLength: 3,
+                                            maxLength: 500
+                                        })
+                                    } onChange={(e) => {
+                                        setArticleMDescr(e.target.value);
+                                    }}/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-sm-auto">
+                                <div className="errordiv text-danger mb-2">
+                                    {errors?.articleMDescr?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a main description of your article.</p></div>}
+                                    {errors?.articleMDescr?.type === "minLength" && <div><h5>Your article's main description is too short.</h5><p>Your article's main description length must be between 150 and 500 characters.</p></div>}
+                                    {errors?.articleMDescr?.type === "maxLength" && <div><h5>Your article's main description is too long.</h5><p>Your article's main description length must be between 150 and 500 characters.</p></div>}
+                                    {errors?.articleMDescr?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row mt-3 mb-5">
+                            <div className="col-sm-auto">
+                                <input className="btn btn-outline-primary" type="submit" value={"Create Article"}/>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col">
+                               <div className="text-danger">
+                                {ErrorMessage && (
+                                    <p>{ErrorMessage}</p>
+                                )}
+                               </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-                <div className="form-group">
-                    <label>Article name:</label>
-                    <input type="text" className="form-control" {
-                        ...register("articleName", {
-                            required: true,
-                            minLength: 1,
-                            maxLength: 20,
-                            pattern: /^[A-Za-z]+$/i //valószínűleg nincsenek benne ékezetes betűk, javítani kell
-                        })
-                    }onChange={(e) => {
-                        setArticleName(e.target.value);
-                    }}/>
-                    
-                    {errors?.articleName?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a name.</p></div>}
-                    {errors?.articleName?.type === "minLength" && <div><h5>Your article's name is too short.</h5><p>Your article's name length must be between 6 and 20 characters.</p></div>}
-                    {errors?.articleName?.type === "maxLength" && <div><h5>Your article's name is too long.</h5><p>Your article's name length must be between 6 and 20 characters.</p></div>}
-                    {errors?.articleName?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
-                </div>
-                
-                <div className="form-group">
-                    <label>Article small description:</label>
-                    <input type="text" className="form-control" {
-                        ...register("articleSmDescr", {
-                            required: true,
-                            minLength: 1,
-                            maxLength: 100,
-                            pattern: /^[A-Za-z]+$/i //valószínűleg nincsenek benne ékezetes betűk, javítani kell
-                        })
-                    }onChange={(e) => {
-                        setArticleSmDescr(e.target.value);
-                    }}/>
-
-                    {errors?.articleSmDescr?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a small description of your article.</p></div>}
-                    {errors?.articleSmDescr?.type === "minLength" && <div><h5>Your article's small description is too short.</h5><p>Your article's small description length must be between 8 and 100 characters.</p></div>}
-                    {errors?.articleSmDescr?.type === "maxLength" && <div><h5>Your article's small description is too long.</h5><p>Your article's small description length must be between 8 and 100 characters.</p></div>}
-                    {errors?.articleSmDescr?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
-                </div>
-
-                <div className="form-group">
-                    <label>Article main description:</label>
-                    <input type="text" className="form-control" {
-                        ...register("articleMDescr", {
-                            required: true,
-                            minLength: 1,
-                            maxLength: 500,
-                            pattern: /^[A-Za-z]+$/i //valószínűleg nincsenek benne ékezetes betűk, javítani kell
-                        })
-                    }onChange={(e) => {
-                        setArticleMDescr(e.target.value);
-                    }}/>
-
-                    {errors?.articleMDescr?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a main description of your article.</p></div>}
-                    {errors?.articleMDescr?.type === "minLength" && <div><h5>Your article's main description is too short.</h5><p>Your article's main description length must be between 150 and 500 characters.</p></div>}
-                    {errors?.articleMDescr?.type === "maxLength" && <div><h5>Your article's main description is too long.</h5><p>Your article's main description length must be between 150 and 500 characters.</p></div>}
-                    {errors?.articleMDescr?.type === "pattern" && <div><h5>Forbidden character usage.</h5><p>You must use alphabetical characters only.</p></div>}
-                </div>
-
-                <div className="form-group">
-                    <label>Article image:</label>
-                    <input type="url" className="form-control" {
-                        ...register("articleImg", {
-                            required: true, //kötelező legyen?
-                            minLength: 1, //Mennyi legyen?
-                            maxLength: 500, //Mennyi legyen?
-                        })
-                    }onChange={(e) => {
-                        setArticleImg(e.target.value);
-                    }}/>
-
-                    {errors?.articleImg?.type === "required" && <div><h5>This field is required!</h5><p>Your article must have a picture</p></div>}
-                    {errors?.articleImg?.type === "minLength" && <div><h5>Your article's picture URL is too short.</h5><p>Your article's picture URL length must be between 150 and 500 characters.</p></div>}
-                    {errors?.articleImg?.type === "maxLength" && <div><h5>Your article's picture URL is too long.</h5><p>Your article's picture URL length must be between 150 and 500 characters.</p></div>}
-                </div>
-
-                <input type="submit" /> {/*Kell egybe ellenörző, küldő gomb vagy külön-külön ha nem megy egybe */}
-            </form>
-
-            <h1>{LoginStatus}</h1>
+            )}
         </div>
     );
 }
